aa4966fb94c67f85830b6bd867b932bf4ea86ed5
jossimar.avirama <jossimar.avirama@sitis.com.co>
2022-04-05 14:41:26 -0500

    create proyect

---
+++ src/main/java/com/fcdevelopment/clinicalagenda/ClinicalAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.fcdevelopment.clinicalagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalAgendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcdevelopment/clinicalagenda/ClinicalAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcdevelopment.clinicalagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
